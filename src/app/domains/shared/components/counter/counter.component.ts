93286188ae55f48d13d80dc29b7ee8413c8b2def
JP <alias99002@outlook.com>
2023-11-28 18:42:16 -0500

    detecting changes from inputs

--- src/app/domains/shared/components/counter/counter.component.ts
+++ src/app/domains/shared/components/counter/counter.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, SimpleChange } from '@angular/core';
+import { Component, Input, SimpleChanges, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
@@ -11,18 +11,58 @@ import { CommonModule } from '@angular/common';
 export class CounterComponent {
 @Input({ required: true }) duration = 0;
 @Input({ required: true }) message = '';
+counter = signal(0);
+counterRef: number | undefined;
 
 constructor() {
   // NO ASYNC
-  // Icalled before render
+  // Is called before render
   console.log('constructor');
   console.log('-'.repeat(10));
 }
 
-ngOnChanges(changes: SimpleChange) {
+ngOnChanges(changes: SimpleChanges) {
   console.log('ngOnChanges');
   console.log('-'.repeat(10));
   console.log(changes);
+
+  // the braces notation because This notation allows you to access properties dynamically
+  const duration = changes['duration'];
+  // by using a variable or a string containing the property's name
+
+  if(duration && duration.currentValue !== duration.previousValue) {
+    this.doSomething()
+  }
+}
+ngOnInit() {
+  // after render
+  // onces
+  //async, then, subs
+  console.log('ngOnInit');
+  console.log('-'.repeat(10));
+  console.log('duration =>', this.duration);
+  console.log('message =>', this.message);
+}
+
+ngAfterViewInit() {
+  // after render
+  //after render children components
+  console.log('ngAfterViewInit');
+  console.log('-'.repeat(10));
+  this.counterRef = window.setInterval(() => {
+    console.log('run interval');
+    this.counter.update(prevState => prevState + 1)
+  }, 1000);
+}
+
+ngOnDestroy() {
+  console.log('ngOnDestroy');
+  console.log('-'.repeat(10));
+  window.clearInterval(this.counterRef);
+}
+
+doSomething() {
+  console.log('doing something');
 }
 
 }

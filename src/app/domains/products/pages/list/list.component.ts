e4f07a3450f3319c93023a803e5786160ed6f03b
JP <alias99002@outlook.com>
2023-12-02 11:40:51 -0500

    fix bug  at cartService

--- src/app/domains/products/pages/list/list.component.ts
+++ src/app/domains/products/pages/list/list.component.ts
@@ -1,8 +1,9 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProductComponent } from '../../components/product/product.component';
 import { Product } from '../../../shared/models/product.model'
 import { HeaderComponent } from '../../../shared/components/header/header.component';
+import { CartService } from '../../../shared/services/cart.service';
 
 @Component({
   selector: 'app-list',
@@ -13,8 +14,7 @@ import { HeaderComponent } from '../../../shared/components/header/header.compon
 })
 export class ListComponent {
   products = signal<Product[]>([]);
-  cart = signal<Product[]>([]);
-
+  cartService = inject(CartService);
   constructor() {
     const initProducts: Product[] = [
       {
@@ -64,7 +64,7 @@ export class ListComponent {
   }
 
   addToCart(product: Product) {
-   this.cart.update(prevState => [...prevState, product])
+   this.cartService.addToCart(product);
   }
 
 

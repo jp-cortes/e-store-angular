dd2fa78e9dd58d95f39c33563089a097979e7a9d
Juan Cortes <code_alias@outlook.com>
2023-12-27 16:29:59 +0200

    fix bug at intercerptor authToken

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,13 +2,12 @@ import { ApplicationConfig } from '@angular/core';
 import { PreloadAllModules, provideRouter, withComponentInputBinding, withPreloading } from '@angular/router';
 
 import { routes } from './app.routes';
-import { HTTP_INTERCEPTORS, provideHttpClient, withInterceptors } from '@angular/common/http';
-import { TokenInterceptor } from './interceptors/token.interceptor';
-import { authtokenInterceptor } from './interceptors/authtoken.interceptor';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { authTokenInterceptor } from './interceptors/authtoken.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes, withComponentInputBinding(), withPreloading(PreloadAllModules)),
-    provideHttpClient(withInterceptors([authtokenInterceptor])),
+    provideHttpClient(withInterceptors([authTokenInterceptor])),
   ]
 };
--- src/app/interceptors/authtoken.interceptor.ts
+++ src/app/interceptors/authtoken.interceptor.ts
@@ -1,14 +1,30 @@
 import { HttpInterceptorFn, HttpRequest } from '@angular/common/http';
+import { inject } from '@angular/core';
 import { AuthTokenService } from '@shared/services/auth-token.service';
-import { CookieService } from 'ngx-cookie-service';
 
-export const authtokenInterceptor: HttpInterceptorFn = (req, next) => {
-  // req = addHeaders(req);
-  // const authToken = AuthTokenService.prototype.getToken();
-
-
+export const authTokenInterceptor: HttpInterceptorFn = (req, next) => {
+  req = addHeaders(req);
   return next(req)
 };
 
+const addHeaders = (request: HttpRequest<any>) => {
+  // Get the auth token from the service.
 
+  const authToken = inject(AuthTokenService);
+
+  const token = authToken.getToken();
+
+    if (token) {
+      // Clone the request and replace the original headers with
+  // cloned headers, updated with the authorization.
+  const authReq = request.clone({
+    headers: request.headers.set('Authorization',`Bearer ${token}`)
+
+  });
+  return authReq
+
+    }
+    return request;
+
+  }
 

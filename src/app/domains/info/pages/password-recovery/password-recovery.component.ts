854f5370fb5239bd397865f04662ee198c8a58ab
Juan Cortes <code_alias@outlook.com>
2024-01-06 18:48:06 +0200

    bug at recoveryEmail service

--- src/app/domains/info/pages/password-recovery/password-recovery.component.ts
+++ src/app/domains/info/pages/password-recovery/password-recovery.component.ts
@@ -2,7 +2,7 @@ import { Component, Input, inject, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Router, RouterLinkWithHref } from '@angular/router';
 import { UserService } from '@shared/services/user.service';
-import { FormsModule } from '@angular/forms';
+import { FormControl, FormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-password-recovery',
@@ -13,22 +13,34 @@ import { FormsModule } from '@angular/forms';
 })
 export class PasswordRecoveryComponent {
   emailSent = signal(false);
+  inValidEmail = signal(false);
   private router = inject(Router)
   private userService = inject(UserService);
   @Input() email!: string;
 
 
+
   sendRecoveryEmail() {
     const email = this.email;
-    if(email) {
+    const regex = new RegExp(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/);
+    const isValid = regex.test(email)
+
+    if(isValid) {
       this.userService.sendRecoveryEmail(email)
       .subscribe({
         next: () => {
           this.emailSent.update( prevState => !prevState);
-          this.router.navigate(['/sign-in']);
+          setTimeout(() => {
+            this.router.navigate(['/sign-in']);
+          }, 3000)
         },
-        error: (error) => console.log(error, 'error at userService sendRecoveryEmail()'),
+        error: (error) => {
+          console.log(error, 'error at userService sendRecoveryEmail()')
+          this.inValidEmail.set(true);
+        },
       });
+    } else {
+      this.inValidEmail.set(true);
     }
   }
 }

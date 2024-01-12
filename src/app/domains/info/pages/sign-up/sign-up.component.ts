5fe99691b6840a0733719c755fc9d5ab11e9677d
Juan Cortes <code_alias@outlook.com>
2024-01-12 23:22:25 +0200

    adding a reactive form to signUpComponent

--- src/app/domains/info/pages/sign-up/sign-up.component.ts
+++ src/app/domains/info/pages/sign-up/sign-up.component.ts
@@ -65,23 +65,70 @@ export class SignUpComponent {
     }),
   },);
 
-  onInit() {
+  onInit() { }
 
+
+
+
+  signUp(event: Event) {
+    this.newProfileForm.markAllAsTouched()
+
+    const {name, lastName,
+      phone, user} = this.newProfileForm.value;
+    console.log(name)
+    console.log(lastName)
+    console.log(phone)
+    console.log(user?.email)
+    console.log(user?.password)
+    console.log(user?.confirm_password)
+    console.log(this.newProfileForm.valid)
   }
 
-  passwordMatchValidator() {
-    return this.newProfileForm.value.user?.password === this.newProfileForm.value.user?.confirm_password
-       ? true : null;
+
+// getting the inputs
+  get nameField() {
+    return this.newProfileForm.get('name')
+  }
+  get lastNameField() {
+    return this.newProfileForm.get('lastName')
+  }
+  get phoneField() {
+    return this.newProfileForm.get('phone')
+  }
+  get emailField() {
+    return this.newProfileForm.get('user.email')
+  }
+  get passwordField() {
+    return this.newProfileForm.get('user.password')
+  }
+  get confirmPasswordField() {
+    return this.newProfileForm.get('user.confirm_password')
+  }
+
+// validators
+  get invalidNameField() {
+    return this.nameField?.touched && this.nameField?.invalid
+  }
+  get invalidLastNameField() {
+    return this.lastNameField?.touched && this.lastNameField?.invalid
+  }
+  get invalidPhoneField() {
+    return this.phoneField?.touched && this.phoneField?.invalid
+  }
+  get invalidEmailField() {
+    return this.emailField?.touched && this.emailField?.invalid
+  }
+  get invalidPasswordField() {
+    return this.passwordField?.touched && this.passwordField?.invalid
+  }
+  get invalidConfirmPasswordField() {
+    return this.confirmPasswordField?.touched && this.confirmPasswordField?.invalid
+  }
+
+  get passwordMatchValidator() {
+    return this.passwordField?.value === this.confirmPasswordField?.value
+       ? true : false;
  }
 
 
-  signUp() {
-    console.log(this.newProfileForm.value.name)
-    console.log(this.newProfileForm.value.lastName)
-    console.log(this.newProfileForm.value.phone)
-    console.log(this.newProfileForm.value.user?.email)
-    console.log(this.newProfileForm.value.user?.password)
-    console.log(this.newProfileForm.value.user?.confirm_password)
-    console.log(this.newProfileForm.valid)
-  }
 }

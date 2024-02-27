3a00b055be5653d59b104f01ed7264487ab77aa9
Juan Cortes <code_alias@outlook.com>
2024-02-27 19:37:37 +0200

    add test for getCategories

--- src/app/domains/products/pages/list/list.component.spec.ts
+++ src/app/domains/products/pages/list/list.component.spec.ts
@@ -4,6 +4,8 @@ import { ProductService } from "@shared/services/product.service";
 import { ProductComponent } from "@products/components/product/product.component";
 import { generateProducts } from "@shared/models/product.mock";
 import { of } from "rxjs";
+import { CategoryService } from "@shared/services/category.service";
+import { generateCategories } from "@shared/models/category.mock";
 
 
 
@@ -12,14 +14,17 @@ fdescribe('listComponent',  () => {
 let component: ListComponent;
 let fixture: ComponentFixture<ListComponent>;
 let productService: jasmine.SpyObj<ProductService>;
+let categoryService: jasmine.SpyObj<CategoryService>;
 
 beforeEach(async () => {
   const productServiceSpy = jasmine.createSpyObj('ProductService', ['getProducts']);
+  const categoryServiceSpy = jasmine.createSpyObj('CategoryService', ['getCategories']);
 
 await TestBed.configureTestingModule({
   imports: [ ProductComponent, ListComponent],
   providers: [
     { provide: ProductService, useValue: productServiceSpy },
+    { provide: CategoryService, useValue: categoryServiceSpy },
   ]
 })
 .compileComponents();
@@ -30,9 +35,13 @@ beforeEach(() => {
   fixture = TestBed.createComponent(ListComponent);
   component = fixture.componentInstance;
   productService = TestBed.inject(ProductService) as jasmine.SpyObj<ProductService>;
+  categoryService = TestBed.inject(CategoryService) as jasmine.SpyObj<CategoryService>;
 
   const productsMock = generateProducts(3);
+  const categoriesMock = generateCategories(3);
+
   productService.getProducts.and.returnValue(of(productsMock));
+  categoryService.getCategories.and.returnValue(of(categoriesMock));
   fixture.detectChanges(); // OnInit
 });
 
@@ -41,7 +50,7 @@ it('should create ListComponent', () => {
   expect(productService.getProducts).toHaveBeenCalled();
 });
 
-xdescribe('Test for getProducts', () => {
+describe('Test for getProducts', () => {
   it('Should return a list of products', () => {
     // Arrange
     const productsMock = generateProducts();
@@ -52,6 +61,20 @@ xdescribe('Test for getProducts', () => {
     fixture.detectChanges();
     // Assert
     expect(component.products.length).toEqual(productsMock.length + countPrev);
+  });
+});
+
+describe('Test for getCategories', () => {
+  it('Should return a list of categories', () => {
+    // Arrange
+    const categoriesMock = generateCategories(3);
+    categoryService.getCategories.and.returnValue(of(categoriesMock));
+
+    // Act
+    component.getCategories();
+    fixture.detectChanges();
+    // Assert
+    expect(component.products.length).toEqual(categoriesMock.length);
   });
 });
 

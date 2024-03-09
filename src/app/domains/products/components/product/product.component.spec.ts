0083d0a163d24fa327df42480611a8f77c7c9524
jp-cortes <code_alias@outlook.com>
2024-03-09 21:23:36 +0200

    fix bug at test of ProductComponent

--- src/app/domains/products/components/product/product.component.spec.ts
+++ src/app/domains/products/components/product/product.component.spec.ts
@@ -1,8 +1,9 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { ProductComponent } from './product.component';
+import { generateOneProduct } from '@shared/models/product.mock';
 
-xdescribe('ProductComponent', () => {
+describe('ProductComponent', () => {
   let component: ProductComponent;
   let fixture: ComponentFixture<ProductComponent>;
 
@@ -14,8 +15,10 @@ xdescribe('ProductComponent', () => {
   });
 
   beforeEach(() => {
+    const product = generateOneProduct();
     fixture = TestBed.createComponent(ProductComponent);
     component = fixture.componentInstance;
+    component.product = product;
     fixture.detectChanges();
   });
 

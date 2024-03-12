d030f1b88014b5e6a03ed1ec46e29136f9d68db6
jp-cortes <code_alias@outlook.com>
2024-03-13 01:50:45 +0200

    Testing buttons & rendering

--- src/app/domains/shared/components/menu-mobile/menu-mobile.component.spec.ts
+++ src/app/domains/shared/components/menu-mobile/menu-mobile.component.spec.ts
@@ -4,6 +4,8 @@ import { MenuMobileComponent } from './menu-mobile.component';
 import { CategoryService } from '@shared/services/category.service';
 import { generateCategories } from '@shared/models/category.mock';
 import { of } from 'rxjs';
+import { By } from '@angular/platform-browser';
+import { DebugElement } from '@angular/core';
 
 describe('Test for MenuMobileComponent', () => {
   let component: MenuMobileComponent;
@@ -35,7 +37,63 @@ describe('Test for MenuMobileComponent', () => {
     fixture.detectChanges();
 });
 
-  it('should create', () => {
+
+
+it('should create', () => {
     expect(component).toBeDefined();
+    expect(categoryService.getCategories).toHaveBeenCalled();
   });
+
+  it('Should display the text "Menu" in tag h5', () => {
+    // Arrange
+    const h5De: DebugElement = fixture.debugElement.query(By.css('h5.text-base'));
+    const h5El: HTMLElement = h5De.nativeElement;
+    // Act
+    fixture.detectChanges();
+    // Assert
+    expect(h5El.textContent).toContain('Menu');
+  });
+
+  
+  
+  it('Should toggle the signal showSideMenu', () => {
+    const btnOpenDe: DebugElement = fixture.debugElement.query(By.css('div.relative button'));
+    const btnCloseDe: DebugElement = fixture.debugElement.query(By.css('div.fixed button.bg-transparent '));
+
+    // Act
+    //this click should change the signal showSideMenu() false
+    btnOpenDe.triggerEventHandler('click', null);
+
+    fixture.detectChanges();
+    // Assert
+    expect(component.showSideMenu()).toBe(false);
+
+    // this click should change the signal showSideMenu() to true
+    btnCloseDe.triggerEventHandler('click', null);
+    fixture.detectChanges();
+
+    expect(component.showSideMenu()).toBe(true);
+  });
+
+  
+  
+  it('Should return a list of categories', () => {
+    // Arrange
+    const categoriesMock = generateCategories(3);
+    categoryService.getCategories.and.returnValue(of(categoriesMock));
+    const liDe = fixture.debugElement.query(By.css('div > ul > li.mt-4 > a'));
+    const liEl: HTMLElement = liDe.nativeElement;
+
+    // Act
+    component.getCategories();
+
+    expect(liEl).toBeDefined();
+
+    fixture.detectChanges();
+    // Assert
+    expect(component.categories().length).toEqual(categoriesMock.length);
+    expect(component.categories()).toEqual(categoriesMock)
+  });
+
+
 });

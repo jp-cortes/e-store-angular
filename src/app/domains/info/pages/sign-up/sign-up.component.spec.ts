8bd191da4a812d4a4b463bdc9a0e9ff7707e758c
jp-cortes <code_alias@outlook.com>
2024-03-31 23:25:56 +0300

    Test for SignUpComponent

---
+++ src/app/domains/info/pages/sign-up/sign-up.component.spec.ts
@@ -0,0 +1,46 @@
+import { ComponentFixture, TestBed } from "@angular/core/testing";
+import { SignUpComponent } from "./sign-up.component";
+import { UserService } from "@shared/services/user.service";
+import { RouterTestingModule } from "@angular/router/testing";
+import { generateCustomer, generateSignUpCustomer } from "@shared/models/user.mock";
+import { of } from "rxjs";
+
+
+
+describe('Test for SignUpComponent', () => {
+    let component: SignUpComponent;
+    let fixture: ComponentFixture<SignUpComponent>;
+    let userService: jasmine.SpyObj<UserService>;
+  
+    beforeEach(async () => {
+      const userServiceSpy = jasmine.createSpyObj('UserService', ['signUp']);
+  
+      await TestBed.configureTestingModule({
+        imports: [ SignUpComponent, RouterTestingModule ],
+        providers: [
+          { provide: UserService, useValue: userServiceSpy },
+        ]
+      })
+      .compileComponents();
+    });
+  
+    beforeEach(() => {
+      fixture = TestBed.createComponent(SignUpComponent);
+      userService = TestBed.inject(UserService) as jasmine.SpyObj<UserService>;
+
+      const customerMock = generateCustomer();
+      
+      userService.signUp.and.returnValue(of(customerMock));
+  
+      component = fixture.componentInstance;
+  
+      fixture.detectChanges();
+    });
+
+    it('Should create SignUpComponent', () => {
+        fixture.detectChanges();
+        expect(component).toBeDefined();
+    });
+
+
+});

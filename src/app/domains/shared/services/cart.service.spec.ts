9c14d11c5ac0a03f74a71ab2d1e789b5257bfb30
jp-cortes <code_alias@outlook.com>
2024-04-23 00:01:24 +0300

    update test at CartService

--- src/app/domains/shared/services/cart.service.spec.ts
+++ src/app/domains/shared/services/cart.service.spec.ts
@@ -1,27 +1,47 @@
 import { generateCartItems } from "@shared/models/cart.mock";
 import { CartService } from "./cart.service"
 import { TestBed } from "@angular/core/testing";
+import { generateOneProduct } from "@shared/models/product.mock";
 
-describe('Test for  Cartservice', () => {
-    let cartService: CartService;
+fdescribe('Test for  Cartservice', () => {
+    let cartService: jasmine.SpyObj<CartService>;
 
     beforeEach(() => {
+        const cartServiceSpy = jasmine.createSpyObj('CartService', ['addToCart', 'removeFromCart', 'clearCart', 'items', 'subtotal', 'count'])
         TestBed.configureTestingModule({
             providers: [
-                CartService
+                { provide: CartService, useVlue: cartServiceSpy }
             ]
         });
-        cartService =  TestBed.inject(CartService);
+        cartService =  TestBed.inject(CartService) as jasmine.SpyObj<CartService>;
+
+        const mockCartItemt = generateOneProduct();
+
+        cartService.addToCart(mockCartItemt);
+    
     });
 
-    it('Should create Cartservice', () => {
+    it('Should create Cartservice and signals should be defined', () => {
         expect(cartService).toBeDefined();
+        expect(cartService.cart()).toBeDefined();
+        expect(cartService.items()).toBeDefined();
+        expect(cartService.count()).toBeDefined();
+        expect(cartService.subtotal()).toBeDefined();
     })
     
     // describe('Test for items in CartService', () => {
 
     // it('Test for items should return an aray of CartItems', () => {
-    //         expect(cartService.items).toBe(generateCartItems());
+    //     const mockCartItem = generateOneProduct();
+    //     const mockCartItems = generateCartItems();
+    //     cartService.addToCart(mockCartItem);
+
+    //     cartService.items.and.returnValue(mockCartItems)
+
+    //     expect(cartService.items).toBeDefined();
+    //     expect(cartService.items).toEqual(mockCartItems)
+    //     // expect(cartService.count).toBeDefined();
+    //     // expect(cartService.subtotal).toBeDefined();
     //     });
     // });
 

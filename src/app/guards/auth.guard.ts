fbd963877e00194900e557f0e5a3fa39bc7ef31f
jp-cortes <code_alias@outlook.com>
2024-05-19 04:37:15 +0300

    adding comments

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -6,11 +6,14 @@ import { UserService } from '@shared/services/user.service';
 export const authGuard: CanActivateFn = (route, state) => {
   const userService = inject(UserService);
   const authTokenService = inject(AuthTokenService);
+  // get token from cookies
   const token = authTokenService.getToken();
 
   if(token) {
+    // if the token exist the user can access the route
     return true;
   }
+  // if the token doesn't exist user will be redireted to login
   userService.redirect('/sign-in');
   return false;
 };
